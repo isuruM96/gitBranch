2a94c53e616da8379218635f478724ea961e6744
CompSc User <cscuser@L01-HP35.cssamba.jfn.ac.lk>
2020-09-18 04:15:47 +0530

    added the csc.example.java file in master

---
+++ CscExamples.java
@@ -0,0 +1,16 @@
+public class cscExamples{
+
+int questionNo;
+string  catagory;
+String text;
+
+Public int setQuestionNo(){
+	this.questionNo=questionNo;
+}
+public String setCatagory(){
+   this.catagory=catagory;	
+} 
+public String setText(){
+	this.text=text;
+}
+}
